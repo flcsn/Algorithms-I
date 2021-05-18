6c165d76ee3bb8441a5f2636c9d815b1954bb2e6
flcsn <73377890+flcsn@users.noreply.github.com>
2021-05-18 23:33:52 +0800

    Refined answers to Introductory exercises

---
+++ RandomWord/src/RandomWord.java
@@ -0,0 +1,24 @@
+
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+
+	public static void main(String[] args) {
+		String champion = "";
+		int wordCount = 0;
+		
+		while (!StdIn.isEmpty()) {
+			String input = StdIn.readString();
+			wordCount++;
+			
+			if (StdRandom.bernoulli(1.0/wordCount)) {
+				champion = input;
+			}
+		}
+		
+		System.out.println(champion);
+	}
+}
