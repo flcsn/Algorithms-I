3da069eeb686089d799e8b96348e1b3af79db5f4
flcsn <73377890+flcsn@users.noreply.github.com>
2021-05-29 14:01:05 +0800

    second crack at Deque

--- Deque/src/main/java/Deque.java
+++ Deque/src/main/java/Deque.java
@@ -1,18 +1,27 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
-	private Item[] items;
 	private int currentNumberOfItems;
+	private Node front;
+	private Node end;
 
     // construct an empty deque
     public Deque() {
-    	this.items = (Item[]) new Object[1];
     	currentNumberOfItems = 0;
+    	front = new Node();
+    	end = front;
+    }
+    
+    private class Node {
+    	Node next;
+    	Item item;
+    	Node previous;
     }
 
     // is the deque empty?
     public boolean isEmpty() {
-    	return currentNumberOfItems == 0;
+    	return front == end;
     }
 
     // return the number of items on the deque
@@ -22,44 +31,89 @@ public class Deque<Item> implements Iterable<Item> {
 
     // add the item to the front
     public void addFirst(Item item) {
-    	items[0] = item;
+    	if (item == null) throw new IllegalArgumentException("item cannot be null");
+    	Node newItem = new Node();
+    	newItem.item = item;
+    	newItem.next = front;
+    	front.previous = newItem;
+    	front = newItem;
+
     	currentNumberOfItems++;
-    	if (currentNumberOfItems == items.length) grow();
     }
 
     // add the item to the back
     public void addLast(Item item) {
-    	items[currentNumberOfItems] = item;
+    	if (item == null) throw new IllegalArgumentException("item cannot be null");
+    	Node newEnd = new Node();
+    	end.item = item;
+    	end.next = newEnd;
+    	newEnd.previous = end;
+    	end = newEnd;
+    	
     	currentNumberOfItems++;
-    	if (currentNumberOfItems == items.length) grow();
-    }
-    
-    private void grow() {
-    	Item[] newArray = (Item[]) new Object[items.length*2];
-    	for (int i = 0; i < items.length; i++) newArray[i] = items[i];
-    	items = newArray;
     }
 
     // remove and return the item from the front
-    //public Item removeFirst()
+    public Item removeFirst() { 
+    	if (this.isEmpty()) throw new NoSuchElementException("deque is empty");
+    	Item frontItem = front.item;
+    	front = front.next;
+    	front.previous = null;
+    	
+    	currentNumberOfItems--;
+    	return frontItem;
+    }
 
     // remove and return the item from the back
-    //public Item removeLast()
+    public Item removeLast() {
+    	if (this.isEmpty()) throw new NoSuchElementException("deque is empty");
+    	end = end.previous;
+    	Item endItem = end.item;
+    	end.next = null;
+    	
+    	currentNumberOfItems--;
+    	return endItem;
+    }
 
     // return an iterator over items in order from front to back
     public Iterator<Item> iterator() {
-    	return new StackIterator();
+    	return new DequeIterator();
     }
     
-    private class StackIterator implements Iterator<Item> {
-    	private int j = 0;
+    private class DequeIterator implements Iterator<Item> {
+    	private Node current = front;
     	
-    	public boolean hasNext() {return j != items.length;}
-    	public void remove() {;}
-    	public Item next() {return items[j++];}
+    	public boolean hasNext() { return current.item != null; }
+    	public void remove() { throw new UnsupportedOperationException("cannot call remove");}
+    	public Item next() {
+    		Item item = current.item;
+    		current = current.next;
+    		return item;
+    	}
     }
 
     // unit testing (required)
-    //public static void main(String[] args)
+    public static void main(String[] args) {
+    	Deque<String> deque = new Deque<String>();
+    	System.out.println(deque);
+    	System.out.println("Adding in order: third, first, second, fourth...");
+		deque.addFirst("first");
+		deque.addLast("second");
+		deque.addFirst("third");
+		deque.addLast("fourth");
+		System.out.println("The deque has " + deque.size() + " nodes.");
+		System.out.println("");
+		System.out.println("From front to end, this deque has the following nodes: ");
+		Iterator<String> iterator = deque.iterator();
+		while (iterator.hasNext()) { System.out.print(iterator.next() + " ");}
+		System.out.println("");
+		System.out.println("At front: " + deque.removeFirst());
+		System.out.println("At end: " + deque.removeLast());
+		System.out.println("The deque now has " + deque.size() + " nodes.");
+		System.out.println("Removing...");
+		System.out.println("At front: " + deque.removeFirst());
+		System.out.println("At end: " + deque.removeLast());
+		System.out.println("The deque now has " + deque.size() + " nodes.");
+    }
 
 }
--- Deque/src/test/java/DequeTest.java
+++ Deque/src/test/java/DequeTest.java
@@ -1,5 +1,11 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class DequeTest {
@@ -31,22 +37,89 @@ public class DequeTest {
 	}
 	
 	@Test
-	public void arrayGrowsWhenMoreItemsAreAdded() {
+	public void addingNullThrowsIllegalArgumentException() {
 		Deque<String> deque = new Deque<String>();
-		deque.addLast("first string added");
-		deque.addFirst("second string added");
-		deque.addFirst("third string added");
-		assertTrue(deque.size() == 3);
+		Assert.assertThrows(IllegalArgumentException.class, () -> {
+			deque.addFirst(null);
+		});
+		Assert.assertThrows(IllegalArgumentException.class, () -> {
+			deque.addLast(null);
+		});
 	}
 	
-	
-	/*@Test
-	public void addFirstAddsItemToFront() {
+	@Test
+	public void removeFirstRemovesFirstItem() {
 		Deque<String> deque = new Deque<String>();
 		deque.addFirst("first string added");
 		deque.addFirst("second string added");
-		assertEquals("second string added", deque.items[0]);
-	}*/
+		assertTrue(deque.removeFirst().equals("second string added"));
+		assertTrue(deque.removeFirst().equals("first string added"));
+	}
+	
+	@Test
+	public void removeLastRemovesLastItem() {
+		Deque<String> deque = new Deque<String>();
+		deque.addLast("first string added");
+		deque.addLast("second string added");
+		assertTrue(deque.removeLast().equals("second string added"));
+		assertTrue(deque.removeLast().equals("first string added"));
+	}
+	
+	@Test
+	public void removingWhenDequeIsEmptyThrowsNoSuchElementException() {
+		Deque<String> deque = new Deque<String>();
+		Assert.assertThrows(NoSuchElementException.class, () -> {
+			String item = deque.removeFirst();
+		});
+		Assert.assertThrows(NoSuchElementException.class, () -> {
+			String item = deque.removeLast();
+		});
+	}
+	
+	@Test
+	public void addingAndRemovingMultipleItemsConsecutively() {
+		Deque<String> deque = new Deque<String>();
+		deque.addFirst("first");
+		deque.addLast("second");
+		deque.addFirst("third");
+		deque.addLast("fourth");
+		assertTrue(deque.size() == 4);
+		assertTrue(deque.removeLast().equals("fourth"));
+		assertTrue(deque.removeFirst().equals("third"));
+		assertTrue(deque.removeLast().equals("second"));
+		assertTrue(deque.removeFirst().equals("first"));
+		assertTrue(deque.size() == 0);
+		assertTrue(deque.isEmpty());
+	}
+	
+	@Test
+	public void dequeIteratorDoesNotIterateOnEmptyDeque() {
+		Deque<String> deque = new Deque<String>();
+		Iterator<String> iterator = deque.iterator();
+		assertFalse(iterator.hasNext());
+	}
+	
+	@Test
+	public void dequeIteratorIterates() {
+		Deque<String> deque = new Deque<String>();
+		deque.addFirst("first");
+		deque.addLast("second");
+		deque.addLast("third");
+		Iterator<String> iterator = deque.iterator();
+		assertTrue(iterator.next().equals("first"));
+		assertTrue(iterator.next().equals("second"));
+		assertTrue(iterator.next().equals("third"));
+		assertNull(iterator.next());
+	}
+	
+	@Test
+	public void dequeIteratorRemoveMethodThrowsException() {
+		Deque<String> deque = new Deque<String>();
+		Iterator<String> iterator = deque.iterator();
+		assertThrows(UnsupportedOperationException.class, () -> {
+			iterator.remove();
+		});
+	}
 	
 
 }
