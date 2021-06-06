ec0723fa48d4fcb2933d529d13580c024df03b53
flcsn <73377890+flcsn@users.noreply.github.com>
2021-06-06 21:14:34 +0800

    fifth crack at collinearPoints

--- collinearPoints/src/main/java/FastCollinearPoints.java
+++ collinearPoints/src/main/java/FastCollinearPoints.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class FastCollinearPoints {
+	private Point[] sortedPoints;
 	private LineSegment[] lineSegments;
 	private int numberOfLineSegments;
 	
@@ -8,34 +9,69 @@ public class FastCollinearPoints {
 	public FastCollinearPoints(Point[] points) {
 		if (points == null) throw new IllegalArgumentException("argument is null");
 		lineSegments = new LineSegment[points.length];
+		sortedPoints = new Point[points.length];
 		numberOfLineSegments = 0;
+		
+		for (int i = 0; i < points.length; i++) {
+			checkIfNull(points[i]);
+			sortedPoints[i] = points[i];
+		}
 		
 		// determine point p
 		for (int p = 0; p < points.length; p++) {
 			Point origin = points[p];
-			checkIfNull(origin);
 			
 			// for every other point q, sort the points according to the slope made from p
-			Arrays.sort(points, origin.slopeOrder());
+			Arrays.sort(sortedPoints, origin.slopeOrder());
 			
 			// compare slope values of adjacent points
-			for (int q = p + 1; q < points.length; q++) {
-				checkIfNull(points[q]);	
-				int count = 0;
+			for (int q = 0; q < sortedPoints.length; q++) {
+				int count = 1;
+				// if origin is the same point as q, disregard loop
+				if (origin.slopeTo(sortedPoints[q]) == Double.NEGATIVE_INFINITY) continue;
+				
 				// if slope values are equal, continue checking next values 
 				// until their slope values are no longer equal
-				for (int i = q + 1; origin.slopeTo(points[q]) == origin.slopeTo(points[i]); i++) {
+				for (int i = q + 1; i < sortedPoints.length; i++) {
 					// count number of adjacent points with equal slopes from p
-					checkIfNull(points[i]);	
-					count++;
+					if (origin.slopeTo(sortedPoints[i]) == Double.NEGATIVE_INFINITY) continue;
+					else if (origin.slopeTo(sortedPoints[q]) == origin.slopeTo(sortedPoints[i])) count++;
+					else break;
 				}
-				// if count of adjacent equal slopes >= 3
-				// return a line segment from p to the last adjacent point with an equal slope
-				if (count >= 3) lineSegments[numberOfLineSegments++] = new LineSegment(origin, points[q+count]);
+				// if count of collinear points from origin >= 3
+				// try to add line segment
+				if (count >= 3) {
+					
+					// look for minimum and maximum points of the line segment
+					Point min = origin;
+					Point max = sortedPoints[q+count-1];
+					
+					// swaps points if min point is greater than max
+					if (min.compareTo(max) > 0) {
+						Point temp = min;
+						min = max;
+						max = temp;
+					}
+					
+					for (int i = q; i <= q+count-1; i++) {
+						if (min.compareTo(sortedPoints[i]) > 0) min = sortedPoints[i];
+						if (max.compareTo(sortedPoints[i]) < 0) max = sortedPoints[i];
+					}
+					
+					// checks array for potential duplicate
+					LineSegment lineSegmentToAdd = new LineSegment(min, max);
+					boolean inArray = false;
+					for (int j = 0; j < numberOfLineSegments; j++) {
+						if (lineSegments[j].toString().equals(lineSegmentToAdd.toString())) inArray = true;
+					}
+					
+					// adds new line segment if no duplicate found
+					if (!inArray) lineSegments[numberOfLineSegments++] = lineSegmentToAdd;
+				}
 				
 				// increment q by the number of equal adjacent points processed, if any
 				// minus 1, to account for the "q++" parameter in the for-loop
-				if (count > 0) q += count-1;
+				q += count-1;
 			}
 			// repeat for N number of points
 		}
--- collinearPoints/src/main/java/Point.java
+++ collinearPoints/src/main/java/Point.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Comparator;
 import edu.princeton.cs.algs4.StdDraw;
